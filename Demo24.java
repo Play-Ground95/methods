e2ca9ddf10066972e7c3a3f894678828c15ff888
Kavindu95 <kavindueng95@gmail.com>
2023-09-01 01:40:50 +0530

    demo24 update

--- Demo24.java
+++ Demo24.java
@@ -1,21 +1,21 @@
 public class Demo24 {
     public static void main(String[] args) {
-        myMethod(10);
+        myMethod(10.5f);
         //1.without boxing unboxing
         //2. box, unbox
         //3.variable numbers of argument arity method
     }
-    static void myMethod(int x){
-        System.out.println("myMethod(int)");
+    // static void myMethod(int x){
+    //     System.out.println("myMethod(int)");
+    // }
+    // static void myMethod(Integer x){
+    //     System.out.println("myMethod(Integer)");
+    // }
+    static void myMethod(Float ...x){
+        System.out.println("myMethod(FL ..x)");
     }
-    static void myMethod(Integer x){
-        System.out.println("myMethod(Integer)");
-    }
-    static void myMethod(Integer ...x){
-        System.out.println("myMethod(Integer ..x)");
-    }
-    static void myMethod(byte ...x){
-        System.out.println("myMethod(int ...x)");
+    static void myMethod(float ...x){
+        System.out.println("myMethod(fl ...x)");
     }
 
 }
